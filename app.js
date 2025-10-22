37883bb0e4eba6f2eb14db9371dc9b34d0624bc4
Abdulrahman436 <arjanbaloch4@gmail.com>
2025-10-22 20:56:53 +0500

    js ch 10 to 14

---
+++ app.js
@@ -0,0 +1,90 @@
+
+
+// ans 1
+
+// let usercity = prompt ("enter your city name")
+
+// if (usercity == "karachi" || usercity == "karachi" ) {
+//     console.log("welcome to city of light")
+// }
+//  else console.log("please enter a valid city name !")
+
+
+// ans 2
+
+// let usergender =prompt ("enter your gender")
+// if (usergender == "male" || usergender == "male")
+//     console.log("Good Morning Sir")
+// else console.log("Good Morning Ma'am")
+
+// ans 3
+
+// let usertrafficcolor =prompt ("Enter Traffic Light Color")
+
+// if (usertrafficcolor == "red")
+//     console.log("Must Stop")
+
+// else if (usertrafficcolor == "yellow")
+//     console.log("Ready to move")
+
+// else if (usertrafficcolor == "green")
+//     console.log("Move now")
+
+
+// ans 4
+
+// let userfuel =+prompt ("Enter reamining fuel")
+
+// if (userfuel < 0.25)
+//     console.log("please refill the fuel in your car")
+
+// else console.log("lets go")
+    
+
+// ans 5
+
+// let a = 4
+// if (++a == 5){
+//     alert("given condition for variable a is true")
+// }
+
+
+// let b = 82
+// if (b++ == 83){
+//     alert("given condition for variable a is true")
+// }
+
+
+// let c = 12
+// if (c++ == 13){
+//     alert("condition 1 is true")
+// }
+// if (c = 13){
+//     alert("condition 2 is true")
+// }
+// if(++c < 14){
+//     alert("condition 3 is true")
+// }
+// if(c == 14){
+//     alert("condition 4 is true")
+// }
+
+
+
+let materialcost = 20000;
+let laborcost = 2000;
+let totalcost = materialcost + laborcost;
+if (totalcost == laborcost + materialcost){
+    alert("the cost equal");
+}
+
+if (true){
+    alert("true")
+}
+if (false){
+    alert("false")
+}
+
+if ("car" < "cat"){
+    alert("car is smaller than cat")
+}
