cd64de223b0c8b95a75361a897534ed54534305b
Abdulrahman436 <arjanbaloch4@gmail.com>
2025-10-24 12:14:38 +0500

    js chp 1 to 13

--- app.js
+++ app.js
@@ -71,20 +71,130 @@
 
 
 
-let materialcost = 20000;
-let laborcost = 2000;
-let totalcost = materialcost + laborcost;
-if (totalcost == laborcost + materialcost){
-    alert("the cost equal");
-}
+// let materialcost = 20000;
+// let laborcost = 2000;
+// let totalcost = materialcost + laborcost;
+// if (totalcost == laborcost + materialcost){
+//     alert("the cost equal");
+// }
 
-if (true){
-    alert("true")
-}
-if (false){
-    alert("false")
-}
+// if (true){
+//     alert("true")
+// }
+// if (false){
+//     alert("false")
+// }
 
-if ("car" < "cat"){
-    alert("car is smaller than cat")
-}
+// if ("car" < "cat"){
+//     alert("car is smaller than cat")
+// }
+
+
+
+// ans 6
+
+// let totalmarks =100
+// let obtainedmarkssub1 = +prompt ("enter your obtained marks sub 1")
+// let obtainedmarkssub2 = +prompt ("enter your obtained marks sub 2")
+// let obtainedmarkssub3 = +prompt ("enter your obtained marks sub 3")
+// let obtmarks = obtainedmarkssub1 + obtainedmarkssub2 + 
+// obtainedmarkssub3
+
+// totalmarks = 100*3
+// let per = (obtmarks/totalmarks)*100
+
+// console.log(per,'%')
+
+// document.writeln("<h1>mark sheet </h1>")
+// document.writeln("total marks =" , totalmarks)
+// document.writeln("<br> obtainedmarks",obtmarks)
+// document.writeln("<br> percentage", per,'%')
+
+// if (per >= 70){
+//     let grade = "A"
+//     let remarks = "good"
+//     document.writeln("<br> grade =",grade)
+//     document.writeln("<br> remarks =",remarks)
+// }
+
+// else if (per < 40){
+//     let grade = "fail"
+//     let remarks = "Sorry"
+//     document.writeln("<br> grade =",grade)
+//     document.writeln("<br> remarks =",remarks)
+// }
+
+
+
+// ans 7
+
+// let scretnum =  5
+// let usernum =+prompt ("guess number 1 to 10")
+
+// if(usernum == scretnum){
+//     console.log("correct guess")
+// }
+// else if (usernum == scretnum -1 || usernum == scretnum +1){
+//     console.log("bas ek step dur")
+// }
+// else if (usernum == scretnum -2 || usernum == scretnum +2){
+//     console.log("thora sa dur bas")
+// }
+// else if (usernum == scretnum -3 || usernum == scretnum +3){
+//     console.log("thoda sa aur try karo")
+// }
+// else if (usernum == scretnum -4 || usernum == scretnum +4 ){
+//     console.log("bohot dur ho")
+// }
+// else {
+//     console.log("plz try again !")
+// }
+
+
+
+// ans 8
+
+// let d = prompt("enter number")
+// e = d % 3
+// if(e == 0){
+//     alert(d+"not divisble by 3")
+// }
+// else{
+//     alert(d+ "not divisble by 3")
+// }
+
+
+// ans 9
+
+// let even = prompt("enter a number")
+
+// if(even % 2 == 0){
+//     alert("even number")
+//     }
+
+//     else{
+//         alert("odd number")
+//     }
+
+
+
+// ans 10
+
+// let temperature = prompt("enter temperature")
+
+// if(temperature >= 40){
+//     console.log("it too hot outside")
+// }
+
+// else if(temperature >= 30){
+//     console.log("the weather today is normal")
+// }
+
+// else if(temperature >= 20){
+//     console.log("today weather is cool")
+// }
+
+// else if(temperature >= 10){
+//     console.log("OMG today weather is so cool")
+// }
+
